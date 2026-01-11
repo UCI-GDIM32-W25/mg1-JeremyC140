f2fd00515b4a45d5247fc9902d88ed7e8be0f682
cAp0103 <jeremychang136@gmail.com>
2026-01-11 00:01:06 -0800

    scripts built

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -13,16 +13,29 @@ public class Player : MonoBehaviour
 
     private void Start ()
     {
-        
+        _numSeedsLeft = _numSeeds;
+        _numSeedsPlanted = 0;
+        _plantCountUI.UpdateSeeds(_numSeedsLeft, _numSeedsPlanted);
     }
 
     private void Update()
     {
-        
+        float vertical = Input.GetAxis("Vertical");
+        _playerTransform.position += Vector3.up * vertical * _speed * Time.deltaTime;
+        float horizontal = Input.GetAxis("Horizontal");
+        _playerTransform.position += Vector3.right * horizontal * _speed * Time.deltaTime;
+        if (Input.GetKeyDown(KeyCode.Space))
+            PlantSeed();
     }
 
     public void PlantSeed ()
     {
-        
+        if (_numSeedsLeft > 0)
+        {
+            _numSeedsLeft -= 1;
+            _numSeedsPlanted += 1;
+            _plantCountUI.UpdateSeeds(_numSeedsLeft, _numSeedsPlanted);
+            Instantiate(_plantPrefab, _playerTransform.position, Quaternion.identity);
+        }
     }
 }
